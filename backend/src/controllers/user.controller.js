42ea88bc07036de614be1ff52ab41672978fe024
Pranav Chopade <pranavchopade129@gmail.com>
2026-06-12 18:18:10 +0530

    fix: update SameSite cookie attribute for production environment

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -25,7 +25,7 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: ENV.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: ENV.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7days
     })
     res.status(201).json({
@@ -60,7 +60,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: ENV.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: ENV.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7days
     })
     res.status(200).json({
